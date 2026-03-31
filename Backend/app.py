9148128e7cecd09960f955140e163909989904a0
Ankit-start <ankitkumarbokaro1@gmail.com>
2026-04-01 03:12:53 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -133,4 +133,5 @@ def overlay():
     return send_file(overlay_path, mimetype="image/png")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # use Render's PORT
+    app.run(host="0.0.0.0", port=port)
